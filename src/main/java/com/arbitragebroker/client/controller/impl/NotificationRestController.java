c321ff26a0311af4ba96abf0d0d18762abd0e330
behrooz <b.mohamadi@extratik.com>
2025-01-19 18:31:18 +0330

    add telegram Notification

--- src/main/java/com/arbitragebroker/client/controller/impl/NotificationRestController.java
+++ src/main/java/com/arbitragebroker/client/controller/impl/NotificationRestController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -50,7 +51,7 @@ public class NotificationRestController extends BaseRestControllerImpl<Notificat
     @PostMapping("/support")
     @ResponseBody
     @Operation(summary = "${api.baseRest.create}", description = "${api.baseRest.create.desc}")
-    public ResponseEntity<NotificationModel> createForSupport(NotificationModel model) {
+    public ResponseEntity<NotificationModel> createForSupport(@RequestBody @Validated NotificationModel model) {
         log.debug("call NotificationRestController.createForSupport for {}", model);
         if (model.getId() != null) {
             throw new BadRequestException("The id must not be provided when creating a new record");
--- src/main/java/com/arbitragebroker/client/model/NotificationModel.java
+++ src/main/java/com/arbitragebroker/client/model/NotificationModel.java
@@ -3,16 +3,21 @@ package com.arbitragebroker.client.model;
 import com.arbitragebroker.client.util.DateUtil;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.springframework.util.StringUtils;
 
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 import java.util.StringJoiner;
 
 @Data
 @Accessors(chain = true)
 public class NotificationModel extends BaseModel<Long> {
+    @NotNull
     private UserModel sender;
     private UserModel recipient;
+    @NotNull
     private String subject;
+    @NotNull
     private String body;
     private boolean read = false;
     private String role;
@@ -35,10 +40,13 @@ public class NotificationModel extends BaseModel<Long> {
 
     @Override
     public String toString() {
-        return """              
-                Sender: %s\n
-                Subject: %s\n               
-                Body: %s\n               
-                """.formatted(sender.getSelectTitle(),subject,body.substring(0,30));
+        StringBuilder builder = new StringBuilder();
+        if(sender!=null)
+            builder.append("Sender: ").append(sender.getSelectTitle()).append("\n");
+        if(StringUtils.hasLength(subject))
+            builder.append("Subject: ").append(subject).append("\n");
+        if(StringUtils.hasLength(body))
+            builder.append("Body: ").append(body, 0, 30);
+       return builder.toString();
     }
 }
